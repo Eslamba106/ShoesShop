3becbdca0e3b2cc264954fcfba23ecaf17b89e8a
Eslamba106 <eslambadawy957@gmail.com>
2023-06-03 02:37:17 +0300

    shoes shop

---
+++ ind.js
@@ -0,0 +1,9 @@
+let img = document.querySelector('.img');
+function eslam(imge){
+    img.src = imge ;
+}
+let container = document.querySelector('.container')
+console.log(container)
+function eslamb(color){
+    container.style.background = color;
+}
